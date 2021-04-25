39c4e2c5918cc2c353f78f888124142f9af6207e
Brian Aguilar D <aguilar.16999@gmail.com>
2021-04-25 18:27:38 -0500

    prueba de leer archivo en vercel

--- pages/api/fechas.js
+++ pages/api/fechas.js
@@ -1,14 +1,18 @@
-import { leerArchivo } from "../../database/utilidades/archivos";
+// import * as path from "path";
+// import { leerArchivo } from "../../database/utilidades/archivos";
 
-const url = "./data/fechas.json";
+import data from "../../data/fechas.json";
+
+// const url = path.join(__dirname, "/data/fechas.json");
+// const url = "./data/fechas.json";
 
 export default async (req, res) => {
   const { method } = req;
 
-  const data = await leerArchivo(url, true);
+  // const data = await leerArchivo(url, true);
 
   if (method === "GET") {
-    res.status(200).json({ status: 200, data });
+    res.status(200).json({ status: 200, data: data });
   } else {
     res.status(404).json({ status: 404, message: "ruta no encontrada" });
   }
