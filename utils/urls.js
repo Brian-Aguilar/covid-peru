bb12d5f6b73e4bbc23d61d5e6ffd37d2a1248dc3
Brian Aguilar D <aguilar.16999@gmail.com>
2021-04-25 17:40:31 -0500

    url de la pagina

--- utils/urls.js
+++ utils/urls.js
@@ -1,6 +1,6 @@
 const urlLocal = "http://localhost:3000";
 const urlIP = "http://192.168.1.9:3000";
-const urlWeb = urlLocal;
+const urlWeb = "https://covid-peru.vercel.app/";
 
 export const urlApi = process.env.NODE_ENV ? urlLocal : urlWeb;
 // export const urlApi = urlIP;
