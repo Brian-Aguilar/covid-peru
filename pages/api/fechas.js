ba9fd2f69319d9e643e1abb68e73bae60b854f5e
Brian Aguilar D <aguilar.16999@gmail.com>
2021-04-25 19:35:53 -0500

    solucion 3 para vercel

--- pages/api/fechas.js
+++ pages/api/fechas.js
@@ -1,9 +1,15 @@
-import * as fs from "fs";
+import * as path from "path";
+
+import { leerArchivo } from "../../database/utilidades/archivos";
+
+const urlData = process.env.NODE_ENV
+  ? "./data/fechas.json"
+  : path.resolve(__dirname, "/data/fechas.json");
 
 export default async (req, res) => {
   const { method } = req;
 
-  const data = JSON.parse(fs.readFileSync("./data/fechas.json").toString());
+  const data = leerArchivo(urlData, true);
 
   if (method === "GET") {
     res.status(200).json({ status: 200, data: data });
