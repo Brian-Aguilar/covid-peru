0ac678b0aa53d2bed9e31291e3c02096fca6f2b4
Brian Aguilar D <aguilar.16999@gmail.com>
2021-04-25 21:53:37 -0500

    configuracion de produccion y desarrollo

--- utils/urls.js
+++ utils/urls.js
@@ -3,5 +3,5 @@ const urlIP = "http://192.168.1.9:3000";
 const urlWeb = "https://covid-peru.vercel.app/";
 
 export const urlApi =
-  process.env.NODE_ENV === "development" ? urlLocal : urlLocal;
+  process.env.NODE_ENV === "development" ? urlLocal : urlWeb;
 // export const urlApi = urlIP;
