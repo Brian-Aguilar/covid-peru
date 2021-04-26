88e2adbf381948b59719f6a7db77212ad4c758a6
Brian Aguilar D <aguilar.16999@gmail.com>
2021-04-25 19:59:16 -0500

    Solucion 5: usando require

--- pages/api/fechas.js
+++ pages/api/fechas.js
@@ -1,22 +1,9 @@
-import * as path from "path";
-
-import { leerArchivo } from "../../database/utilidades/archivos";
-
-const urlData =
-  process.env.NODE_ENV === "development"
-    ? "./data/fechas.json"
-    : path.join(__dirname, "/data/fechas.json");
-
 export default async (req, res) => {
+  const data = require("../../data/fechas.json");
   const { method } = req;
 
-  console.log(process.env.NODE_ENV);
-  console.log(urlData);
-
-  const data = leerArchivo(urlData, true);
-
   if (method === "GET") {
-    res.status(200).json({ status: 200, data: data });
+    res.status(200).json({ status: 200, data });
   } else {
     res.status(404).json({ status: 404, message: "ruta no encontrada" });
   }
